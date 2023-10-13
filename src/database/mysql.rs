3fee73cc4a84e5f97baf2db004f77a3ef320d05d
forDecember <1921567337@qq.com>
2023-10-14 00:06:50 +0800

    update: sqlx

--- src/database/mysql.rs
+++ src/database/mysql.rs
@@ -1,51 +1,42 @@
-use once_cell::sync::OnceCell;
-
-use mysql::{Pool, PooledConn};
-
-static DB_POOL: OnceCell<Pool> = OnceCell::new();
-
-// 初始化数据库链接池
-// #[instrument]
-pub fn init_mysql_pool(db_url: &str) {
-    println!("初始化数据库线程池--------开始-------");
-    DB_POOL
-        .set(mysql::Pool::new(db_url).expect(&format!("Error connecting to {}", &db_url)))
-        .unwrap_or_else(|_| println!("try insert pool cell failure!"));
-    println!("初始化数据库线程池--------结束-------");
-}
-
-// 从链接链接池里面获取链接
-pub fn get_connect() -> PooledConn {
-    println!("从链接池获取数据库链接----------开始----------");
-    let conn = DB_POOL
-        .get()
-        .expect("Error get pool from OneCell<Pool>")
-        .get_conn()
-        .expect("Error get_connect from db pool");
-    println!("从链接池获取数据库链接----------结束----------");
-    conn
-}
-
 #[cfg(test)]
 mod test {
-    use super::*;
-    use chrono::NaiveDate;
-    use mysql::prelude::*;
+
+    use std::time::SystemTime;
+
     use serde::{Deserialize, Serialize};
+    use sqlx::{types::time::PrimitiveDateTime, MySqlPool};
 
     // use super::{get_connect, init_mysql_pool};
 
-    #[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]
+    #[derive(Debug, sqlx::FromRow)]
     struct User {
-        pub id: i64,
-        pub name: String,
-        pub birth: String,
-        pub data: Option<String>,
+        pub id: Option<i32>,
+        pub name: Option<String>,
+        pub birth: Option<String>,
+        pub data: Option<PrimitiveDateTime>,
+    }
+
+    #[test]
+    pub fn test_sqlx() {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async {
+                let pool = MySqlPool::connect("mysql://root:fy@localhost:3306/test1")
+                    .await
+                    .unwrap();
+                let user: User = sqlx::query_as!(User, "SELECT * FROM t_user WHERE id = ?", 1)
+                    .fetch_one(&pool)
+                    .await
+                    .unwrap();
+                println!("{:#?}", user);
+            });
     }
 
     #[test]
     pub fn test_mysql() {
-        // init_mysql_pool("mysql://root:fy@localhost:3306/test1");
+        // init_mysql_pool("");
         // let mut conn = get_connect();
         // let res = conn
         //     .query_map("select * from t_user", |(id, name, birth, data)| User {
