3776450e671dc48ab72e57b0e76ab1e392a2d30c
forDecember <1921567337@qq.com>
2023-11-13 16:39:34 +0800

    update: magic

---
+++ src/api/magic.rs
@@ -0,0 +1,43 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use super::web_utils::get_utils;
+use std::collections::HashMap;
+use std::process;
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct MagicDetails {
+    #[serde(rename = "type")]
+    _type: String,
+    name: String,
+    size: u64,
+    count: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    screenshots: Option<Vec<Value>>,
+}
+
+pub async fn get_preview(url: &str) -> MagicDetails {
+    let ans = get_utils(
+        String::new(),
+        "https://whatslink.info/api/v1/link",
+        vec![("url", url)].into_iter().collect(),
+        HashMap::new(),
+    )
+    .await
+    .unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(0);
+    });
+    let ans: MagicDetails = serde_json::from_str(&ans).unwrap();
+    ans
+}
+
+#[cfg(test)]
+pub mod test {
+
+    use super::*;
+    #[tokio::test]
+    async fn test_get_preview() {
+        get_preview("magnet:?xt=urn:btih:AE5C1273F221FD17D42005F725AF0D596B8C5D4D").await;
+    }
+}
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,5 +2,6 @@ pub mod aitaffy;
 pub mod bilibili;
 pub mod chatgpt;
 pub mod gpt_chat;
+pub mod magic;
 pub mod web_utils;
 pub mod wx_chat;
